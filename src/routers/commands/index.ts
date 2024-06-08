d3e7fcae5b090d12dfc3edb51f17db6a192f20b6
Joseph Carey <josephcareycomposer@gmail.com>
2024-06-08 13:00:13 -0500

    add new slash command routes to support alternative routing for student search

--- src/routers/commands/index.ts
+++ src/routers/commands/index.ts
@@ -1,3 +1,9 @@
+import { camperCommandRegistration } from './camper-command/camper-command.js'
 import { scamprCommandRegistration } from './scampr-command/scampr-command.js'
+import { studentCommandRegistration } from './student-command/student-command.js'
 
-export const routerCommandRegistrations = [scamprCommandRegistration]
+export const routerCommandRegistrations = [
+    scamprCommandRegistration,
+    camperCommandRegistration,
+    studentCommandRegistration,
+]
