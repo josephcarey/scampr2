0d2d8e2b39c36a2a34da9ea424723bb36e6fa264
Joseph Carey <40320948+josephcarey@users.noreply.github.com>
2024-06-09 15:57:43 -0500

    Feature/update camper data script (#15)
    * script for grabbing camper data for 6:15 posting
    
    * order by room number, include preferred name and lessons
    
    * clean up script code
    
    * remove unused await
    
    * add script for generating studio lesson schedules

---
+++ src/scripts/write-lesson-schedules.ts
@@ -0,0 +1,130 @@
+import { getAllCampers } from '../data/fetch-all/fetch-all'
+import 'dotenv/config'
+import { createObjectCsvWriter } from 'csv-writer'
+import { Camper } from '../types'
+
+const OUTPUT_FOLDER = './output/lesson-schedules'
+
+type LessonEntry = {
+    day: string
+    time: string
+    studio: string
+    instrument: string
+    name: string
+}
+
+export type studioTime = {
+    time: string
+    monday: string
+    tuesday: string
+    wednesday: string
+    thursday: string
+    friday: string
+}
+
+const mapLessonEntries: (camper: Camper) => LessonEntry[] = (camper) =>
+    camper.splitLessons.map((lesson) => ({
+        name: camper.preferredName,
+        day: lesson.day,
+        time: lesson.time,
+        studio: lesson.studio,
+        instrument: lesson.instrument,
+    }))
+
+export const main = async () => {
+    const allCampers = await getAllCampers()
+
+    const allLessonEntries = allCampers.flatMap((camper: Camper) =>
+        mapLessonEntries(camper)
+    )
+    console.log(allLessonEntries)
+
+    const studios = new Map()
+
+    // brute force create an entry for each studio
+    for (const lessonEntry of allLessonEntries) {
+        studios.set(lessonEntry.studio, new Map())
+    }
+
+    // add entries for each time represented in the lesson entries
+    for (const lessonEntry of allLessonEntries) {
+        const currentStudio = studios.get(lessonEntry.studio)
+        currentStudio.set(lessonEntry.time, {
+            monday: '',
+            tuesday: '',
+            wednesday: '',
+            thursday: '',
+            friday: '',
+        })
+    }
+
+    // now fill in
+    for (const lessonEntry of allLessonEntries) {
+        const currentStudio = studios.get(lessonEntry.studio)
+        const currentTime = currentStudio.get(lessonEntry.time)
+        switch (lessonEntry.day) {
+            case 'Monday':
+                currentTime.monday = lessonEntry.name
+                break
+            case 'Tuesday':
+                currentTime.tuesday = lessonEntry.name
+                break
+            case 'Wednesday':
+                currentTime.wednesday = lessonEntry.name
+                break
+            case 'Thursday':
+                currentTime.thursday = lessonEntry.name
+                break
+            case 'Friday':
+                currentTime.friday = lessonEntry.name
+                break
+        }
+    }
+
+    // parse these into arrays for sorting and csv input
+    const studioObjects = [...studios].map(([name, value]) => {
+        return {
+            studio: name,
+            times: [...value].map(([name1, value1]) => ({
+                time: name1,
+                ...value1,
+                // days: value1,
+            })),
+        }
+    })
+
+    // sort the sub-arrays
+    for (const studio of studioObjects) {
+        studio.times.sort((a: studioTime, b: studioTime) =>
+            a.time.localeCompare(b.time)
+        )
+    }
+
+    // sort the root array for good measure
+    studioObjects.sort((a, b) => a.studio.localeCompare(b.studio))
+
+    // write the csvs
+    for (const studio of studioObjects) {
+        const path = `${OUTPUT_FOLDER}/${studio.studio}.csv`
+        const csvWriter = createObjectCsvWriter({
+            path,
+            header: [
+                { id: 'time', title: 'Time' },
+                { id: 'monday', title: 'Monday' },
+                { id: 'tuesday', title: 'Tuesday' },
+                { id: 'wednesday', title: 'Wednesday' },
+                { id: 'thursday', title: 'Thursday' },
+                { id: 'friday', title: 'Friday' },
+            ],
+        })
+
+        console.log(`Writing csv ${path}...`)
+        csvWriter
+            .writeRecords(studio.times) // returns a promise
+            .then(() => {
+                console.log('\t...Done')
+            })
+    }
+}
+
+main()
