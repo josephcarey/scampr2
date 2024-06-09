2f02635768ed34a89513e08ec6cb04ee85fa77ab
Joseph Carey <40320948+josephcarey@users.noreply.github.com>
2024-06-08 21:20:33 -0500

    Feature/update camper data script (#14)
    * script for grabbing camper data for 6:15 posting
    
    * order by room number, include preferred name and lessons
    
    * clean up script code
    
    * remove unused await

--- src/scripts/write-camper-data.ts
+++ src/scripts/write-camper-data.ts
@@ -1,7 +1,5 @@
 import { getAllCampers } from '../data/fetch-all/fetch-all'
 import 'dotenv/config'
-// import * as csvWriterPackage from 'csv-writer'
-// const createCsvWriter = csvWriterPackage.createObjectCsvWriter
 import { createObjectCsvWriter } from 'csv-writer'
 import { Camper } from '../types'
 
@@ -9,9 +7,8 @@ const OUTPUT_PATH = './output/camper-data.csv'
 
 export const main = async () => {
     const allCampers = await getAllCampers()
-    allCampers.sort(
-        (a: Camper, b: Camper) =>
-            `${a.lastName}${a.firstName}` > `${b.lastName}${b.firstName}`
+    const sortedCampers = allCampers.sort((a: Camper, b: Camper) =>
+        a.roomNumber.localeCompare(b.roomNumber)
     )
 
     const csvWriter = createObjectCsvWriter({
@@ -19,9 +16,8 @@ export const main = async () => {
         header: [
             { id: 'firstName', title: 'First Name' },
             { id: 'lastName', title: 'Last Name' },
-            // { id: 'preferredName', title: 'Name' },
-            // { id: 'id', title: 'Camper ID' },
-            // { id: 'dateOfBirth', title: 'Date of Birth' },
+            { id: 'preferredName', title: 'Name' },
+            { id: 'roomNumber', title: 'Room Number' },
             { id: 'p1', title: 'P1' },
             { id: 'p2', title: 'P2' },
             { id: 'p3', title: 'P3' },
@@ -36,7 +32,7 @@ export const main = async () => {
     console.log(`Writing csv ${OUTPUT_PATH}...`)
 
     csvWriter
-        .writeRecords(allCampers) // returns a promise
+        .writeRecords(sortedCampers) // returns a promise
         .then(() => {
             console.log('\t...Done')
         })
