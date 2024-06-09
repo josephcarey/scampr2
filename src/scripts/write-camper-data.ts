b9db169124ddea6bea74f8e772ceb9fd0f5b9ef3
Joseph Carey <josephcareycomposer@gmail.com>
2024-06-08 21:19:30 -0500

    clean up script code

--- src/scripts/write-camper-data.ts
+++ src/scripts/write-camper-data.ts
@@ -1,7 +1,5 @@
 import { getAllCampers } from '../data/fetch-all/fetch-all'
 import 'dotenv/config'
-// import * as csvWriterPackage from 'csv-writer'
-// const createCsvWriter = csvWriterPackage.createObjectCsvWriter
 import { createObjectCsvWriter } from 'csv-writer'
 import { Camper } from '../types'
 
@@ -10,7 +8,6 @@ const OUTPUT_PATH = './output/camper-data.csv'
 export const main = async () => {
     const allCampers = await getAllCampers()
     const sortedCampers = allCampers.sort((a: Camper, b: Camper) =>
-        // `${a.lastName}${a.firstName}` > `${b.lastName}${b.firstName}`
         a.roomNumber.localeCompare(b.roomNumber)
     )
 
@@ -21,8 +18,6 @@ export const main = async () => {
             { id: 'lastName', title: 'Last Name' },
             { id: 'preferredName', title: 'Name' },
             { id: 'roomNumber', title: 'Room Number' },
-            // { id: 'id', title: 'Camper ID' },
-            // { id: 'dateOfBirth', title: 'Date of Birth' },
             { id: 'p1', title: 'P1' },
             { id: 'p2', title: 'P2' },
             { id: 'p3', title: 'P3' },
@@ -36,7 +31,7 @@ export const main = async () => {
 
     console.log(`Writing csv ${OUTPUT_PATH}...`)
 
-    csvWriter
+    await csvWriter
         .writeRecords(sortedCampers) // returns a promise
         .then(() => {
             console.log('\t...Done')
